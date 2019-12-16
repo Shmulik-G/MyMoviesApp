fca01245829cb0bc120ae611a6ac9efd8a093eb4
Shmulik-G <Glickmanso@gmail.com>
2019-12-16 23:51:41 +0200

    Create details Activity movie and call the URL youtube link to see the trailer.

---
+++ app/src/main/java/com/shmglickman/mymoviesapp/DetailsActivity.kt
@@ -0,0 +1,22 @@
+package com.shmglickman.mymoviesapp
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_details.*
+
+class DetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+
+        details_btn_trailer.setOnClickListener {
+            val trailerUrl = getString(R.string.infinity_war_trailer)
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(trailerUrl))
+            startActivity(browserIntent)
+        }
+    }
+
+}
